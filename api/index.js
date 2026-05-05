9597759297608a1f021aac5d90d83835541c59b9
Brslex zuli <chaves.123qwe@hotmail.com>
2026-05-05 17:57:15 -0300

    test: simple api

--- api/index.js
+++ api/index.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "meta-llama/llama-3.2-11b-vision-instruct:free",
+          model: "google/gemini-pro-vision",
           messages: [
             {
               role: "system",
